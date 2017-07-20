68fc48a2f6b6c5085a7679192e1b73e8545efec2
max-tweddell <maxt.nz@gmail.com>
2017-07-21 11:42:21 +1200

    updated seed

--- seeds/order_items.js
+++ seeds/order_items.js
@@ -1,12 +1,26 @@
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex('table_name').del()
-        .then(function() {
-            // Inserts seed entries
-            return knex('table_name').insert([
-                { id: 1, Name: 'Goliath Anderson', address: '21 Monggomer st', email: 'dark&stormy@longisland.com', delivery: false, status: '' },
-                { id: 2, colName: 'Peter GRiffin', address: '72 Grogg Lane', email: 'Groggy@bigd.com', delivery: false, status: '' },
-                { id: 3, colName: 'Nickel Sahma', address: '69 Virgina Beach', email: 'virgin@beach.com', delivery: false, status: '' }
-            ]);
-        });
-};
+const uuidv4 = require('uuid/v4')
+const faker = require('faker')
+const knex = require('knex')
+let A = []
+function createData () {
+  Array(23).fill(1).forEach(x => {
+    A.push(new Order(uuidv4 , (faker.name.firstName() + ' ' + faker.name.lastName()), (faker.address.city() + ' ' + faker.address.streetAddress()), faker.internet.email, faker.random.boolean(), faker.random.arrayElement(['delivered','processing','ready'])
+    console.log('beep')
+  })
+}
+function Order (id, name, address, gender, email, delivery) {
+  this.name = name
+  this.address = address
+  this.gender = gender
+  this.email = email
+  this.delivery = delivery
+}
+
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('table_name').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('table_name').insert(A)
+    })
+}
