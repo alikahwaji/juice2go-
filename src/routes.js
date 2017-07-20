872038beb70017ea3f54580311c4dda573617686
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-21 10:24:25 +1200

    adding knex

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
-var db = require('../db')
+var db = require('./db')
 
 router.get('/', function(req, res) {
     res.render('index', { hi: 'Hello World!' })
