2d74efa9230a239bbe36ae4b018862f88206a80b
joshua-fogg <joshuafoggnz@gmail.com>
2017-07-20 17:48:48 +1200

    add db file, views, route templates

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,26 @@
 var express = require('express')
 var router = express.Router()
+var db = require('../db')
 
-router.get('/', function (req, res) {
-  res.render('index', { hi: 'Hello World!' })
+router.get('/', function(req, res) {
+    res.render('index', { hi: 'Hello World!' })
 })
+
+// router.get('/orders', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.get('/order-fulfil/:id', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.get('/orders/new', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.post('/orders/new', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+// router.get('/orders/:id', function(req, res) {
+//     res.render('index', { hi: 'Hello World!' })
+// })
+
 
 module.exports = router
